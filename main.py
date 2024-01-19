a50ee54933bade271f0db4b131aa66d09475b605
XarLinho <131467727+XarLinho@users.noreply.github.com>
2024-01-19 19:47:27 -0300

    Primeiro Spawnpoint

---
+++ main.py
@@ -0,0 +1,123 @@
+#IMPORTAR BIBLIOTECAS E RECURSOS
+import discord
+from discord.ext import commands
+
+import random
+import time
+import locale
+
+from variaveis import ajuda_descricao, exposeds, fanfics, piadas
+from keep_alive import keep_alive
+keep_alive()
+
+import os
+
+#HABILITAR PERMISSÕES
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix='!rz ', intents=intents)
+TOKEN = os.environ['TOKEN']
+
+#COMANDOS
+@bot.command() #!rz ajuda - O bot irá informar ao usuário os comandos que ele atende.
+async def ajuda(ctx):
+    comandos = discord.Embed(
+        title = "*Heya!!* Meus Spawnpoint's:",
+        description = ajuda_descricao,
+        colour = 11598249
+    )    
+    await ctx.reply(embed=comandos)
+
+@bot.command() #!rz dado - O bot irá rodar um dado e dirá o resultado.
+async def dado(ctx, ld:int):
+    lado = random.randint(1,ld)
+    await ctx.reply(f'Caiu no {lado}!')
+
+@bot.command() #!rz dia - O bot irá lhe informar que dia é hoje.
+async def dia(ctx:commands.Context):
+    pessoa = ctx.author
+    locale.setlocale(locale.LC_TIME, 'pt_BR.utf-8')
+    tempo_atual_em_segundos = time.time()
+    tempo_local = time.localtime(tempo_atual_em_segundos)
+    dia = time.strftime("%A", tempo_local)
+    await ctx.reply(f'Hoje é {dia} {pessoa.display_name}.')
+
+@bot.command() #!rz diga (mensagem) - O bot vai repetir o que o user pediu e apagar a msg original.
+async def diga(ctx, *, msg):
+    await ctx.message.delete()
+    await ctx.send(msg)
+
+@bot.command() #!rz exposed - O bot vai lhe informar o motivo de seu exposed.
+async def exposed(ctx, pessoa:discord.Member):
+    exposed = random.choice(exposeds).replace('user', pessoa.display_name)
+    await ctx.reply(exposed)
+
+@bot.command() #!rz fanfic - O bot irá contar um fanfic aleatória do servidor.
+async def fanfic(ctx):
+    fanfic = random.choice(fanfics)
+    await ctx.reply(fanfic)
+
+@bot.command() #!rz hora - O bot irá informar a hora atual.
+async def hora(ctx:commands.Context):
+    pessoa = ctx.author
+    hora = time.strftime('%H:%M', time.localtime())
+    await ctx.reply(f"{pessoa.display_name} Agora são {hora}.")    
+
+@bot.command() #!rz limpar (qt) - O bot irá limpar determinado número de mensagens do chat.
+async def limpar(ctx, quantidade: int):
+    # Verificar se o autor da mensagem tem permissão para limpar mensagens
+    if ctx.message.author.guild_permissions.manage_messages:
+        # Limpar mensagens no canal atual
+        await ctx.channel.purge(limit=quantidade + 1)
+        # Enviar uma mensagem indicando que as mensagens foram limpas
+        await ctx.send(f'{quantidade} mensagens foram removidas.')
+    else:
+        # Se o autor não tiver permissões adequadas, enviar uma mensagem de aviso
+        await ctx.send("Você não tem permissão para limpar mensagens.")
+
+@bot.command() #!rz mandar (id_canal) (fala) - O bot irá mandar o que você disse no canal escolhido.
+async def mandar(ctx, id_canal:int, *, fala):
+    canal = bot.get_channel(id_canal)
+
+    if canal is None:
+        await ctx.send('Canal não encontrado!')
+        return
+    await canal.send(fala)
+
+@bot.command() #!rz piada - O bot irá contar uma piada aleatória sobre Re:Zero.
+async def piada(ctx):
+    piada = random.choice(piadas)
+    await ctx.reply(piada)
+
+@bot.command() #!rz saudação - O bot irá saudar o usuário.
+async def saudar(ctx):
+    user = ctx.author
+    await ctx.reply(f'> Heya {user.display_name}! Aqui é o Barusu. Retornando pela morte estou aqui para encarar qualquer desafio. A vida é cheia de reviravoltas, mas não se preocupe, estou sempre pronto para recomeçar!')
+
+@bot.command() #!rz spam - O bot irá spamar quantas mensagens você quiser no chat.
+async def spam(ctx, qt:int, *, frase):
+    i = 0
+    await ctx.message.delete()
+    for i in range(qt):
+        await ctx.send(frase)
+
+#EVENTOS
+@bot.event #Quando alguem escrever uma mensagem que contenha a palavra rem o usuário o responde.
+async def on_message(pal:discord.Message):
+    await bot.process_commands(pal)
+    if pal.author == bot.user:
+        return
+    if 'rem' in pal.content.lower():
+        await pal.reply('REM??????')
+    elif 'loritta' in pal.content.lower():
+        await pal.reply('Ela quer ser a rainha do humor, mas acho que seu código fonte está mais para uma comédia romântica do que para algum algoritmo eficiente :rofl:.')    
+
+@bot.event #Quando o bot estiver online irá aparecer no console.
+async def on_ready():
+    print(f'{bot.user} está online!')
+    #canal = bot.get_channel(757649361248190546)
+    #await canal.send('Spawnpoint!')
+
+#VALIDAR TOKEN
+bot.run(TOKEN)
---
+++ variaveis.py
@@ -0,0 +1,38 @@
+ajuda_descricao = '''***EVENTOS***\n\n> Quando eu spawnar!\n> Não ouse falar da Rem\n> Loritta?Kkkk\n\n ***COMANDOS***\n\n> **!rz ajuda** - Informo todos os comandos.\n> **!rz dado** (ld) - Irei rolar um dado de quantos lados você quiser!\n> **!rz dia** - Irei lhe informar o dia atual.\n> **!rz diga** (msg) - Envio no chat o que você mandar.\n> **!rz exposed ** @usuário - Irei revelar o segredinho do usuário mencionado.\n> **!rz fanfic** - Conto uma fanfic aleatória do servidor!\n> **!rz hora** - Irei lhe informar a hora atual.\n> **!rz limpar** (qt) - Irei limpar o número de mensagem que pedir.\n> **!rz mandar** (id_canal) (msg) - Mandarei sua mensagem no canal escolhido.\n> **!rz piada** - Conto uma piada aleatória sobre Re:Zero.\n> **!rz saudar** - Me apresento.\n> **!rz spam** (qt) (msg) - spamo o que você quiser quantas vezes quiseres.'''
+
+exposeds = ['''Descobri que o(a) user ainda tem um travesseiro mágico que ele acredita que lhe dá superpoderes durante o sono. Dizem que ele enfrenta dragões e salva o mundo todas as noites!''',
+'''Gente, o(a) user tem uma playlist ultrassecreta chamada 'Músicas para Dançar no Chuveiro'. Dizem as más línguas que ela tem coreografias elaboradas para cada música!''',
+'''Vazou o diário do(a) user durante a quarentena. Parece que ele escrevia cartas de amor para o delivery de comida japonesa. Aparentemente, era um relacionamento sério!''',
+'''O(a) user faz treinamento intensivo de super-herói no quarto. Ele acredita que, em breve, será chamado para integrar a Liga da Justiça. Alguém avisa a ele que Batman já existe?''',
+'''Descobri que o(a) user é uma chef gourmet quando o assunto é micro-ondas. Ela tem receitas elaboradas, como macarrão instantâneo ao molho especial de catchup!''',
+'''A galera não sabia, mas o(a) user é uma dançarina de sapateado nas horas vagas. E ela jura que um dia vai usar isso em uma apresentação surpresa!''',
+'''Gente, o(a) user tem uma coleção de meias exóticas que ele esconde no fundo do armário. Dizem que tem uma meia de abacaxi e outra de dinossauro''',
+'''Vocês não vão acreditar, mas o(a) user é um mestre do air guitar. Ele faz solos épicos quando está sozinho em casa. Parece que vai lançar um álbum solo em breve!''',
+'''O(a) user tem uma consultoria de moda para seu armário colorido. Ela organiza workshops secretos para ensinar as melhores combinações de cores. Quem topa a próxima aula?''',
+'''Descobri que o(a) user é um especialista em teorias da conspiração inofensivas. Ela acredita que os gatos controlam o clima e que os pássaros são espiões do governo. Fiquem de olho nas penas!''']
+
+fanfics = ['''Depois de tomarem banho juntos, Suzanne e Blacky, vão se enxugar no quarto e Suzanne cora, pois Blacky havia deixado a toalha cair* —O que foi gatinha, rsrs? E Blacky vira pra Suzanne. Suzanne desvia o olhar com vergonha —para de provocar daddy! Com um tom de voz vergonhoso Blacky se aproxima lentamente de suzanne, e coloca seu dedo na boca da mesma —shhhhh! Faz silêncio, ou irei te amordaçar, Minha Princesa! Suzanne engole saliva com medo e fala —Ok, Daddy...''',
+'''> *"Tenho medo de te machucar, Fuzin..."* **disse Jonatas**\n> *"Mas eu te amo, não se preocupe se vou me machucar nessa nossa relação... Eu realmente te amo..."***Fusion disse com um sorriso no rosto prestes a chorar**\n> *"Tem certeza mesmo? Você realmente quer satisfazer meus desejos com esse relacionamento..."* **disse Jonatas** **Fusion balançou a cabeça afirmando, mesmo parecendo que estava com medo.**\n> *"Então tá, Fuzin... Mais tarde você vai ter que satisfazer minhas vontades."* **disse Jonatas com um sorriso malicioso no rosto meio corado**''', 
+'''Levir e Kauan eram estudantes dedicados do 3º ano, mas seus caminhos se entrelaçaram apenas quando um projeto interdisciplinar os uniu. Levir, focado em enfermagem, e Kauan, apaixonado por design de interiores, encontraram-se em uma colaboração surpreendente.
+
+Um concurso desafiador propôs a criação de um espaço de convivência inovador para um centro de saúde. Levir, com seu conhecimento em cuidados e bem-estar, buscava integrar elementos terapêuticos. Enquanto isso, Kauan visava uma estética moderna e acolhedora.
+
+No início, suas diferenças criaram atritos, mas conforme trabalhavam juntos, descobriram que a união de suas habilidades resultava em algo extraordinário. Levir trouxe a expertise em criar ambientes funcionais para a promoção da saúde, enquanto Kauan adicionou toques de beleza e conforto.
+
+Ao longo do processo, os dois compartilharam histórias de suas escolas e suas paixões individuais. Levir expressou a importância de compreender as necessidades dos pacientes, enquanto Kauan ressaltou como o design pode influenciar positivamente o estado de espírito das pessoas.
+
+No dia da apresentação, Levir e Kauan surpreenderam os jurados e colegas com um projeto que transcendeu as expectativas. A harmonia entre a funcionalidade da enfermagem e a estética do design de interiores criou um ambiente único que promovia tanto a cura quanto o bem-estar emocional.
+
+A experiência não apenas consolidou uma amizade improvável entre Levir e Kauan, mas também mostrou que, por trás de disciplinas aparentemente distintas, há sempre espaço para a colaboração e a inovação.''',
+'''Ramona havia se acordado de manhã, e foi até a cozinha pegar um suco. Uma voz que ela reconhece fala pra ela por trás dela — Oi gatinha se acordou, quer ovos fritos? *Disse Daniel* Logo após, ela cora, grita e da um tapa em Daniel (ps.: Ele estava sem roupas, apenas de avental de cozinha) Levir se acorda com o grito de daniel — O que caralhos foi isso porra?!''', 
+'''Levir se acordou em mais um dia exaustante de sua vida, deveria ir para a escola em pleno sábado, para ter aula do seu curso de enfermagem. Só iria ele mais 15 alunos dos outros cursos, estava descrente que veria a sua namorada, pois ela mora distante da escola... Ao pisar na sua sala, ele sentiu a brisa fria da sala de aula climatizada, um ar gelado de um ar condicionado, estaria mais ou menos em 15°. Ao sentar na sua cadeira, mãos cobrem a visão do seus olhos: " Adivinha quem é?". Aquela voz era familiar para o menino de estatura baixa. Era uma voz aveludada, macia e meio rouca. Sim, sim, ele certamente conhecia aquela voz. Então, ele pega nas mãos da pessoa que estava com as mãos em seus olhos, estas que estavam meio frias, ele disse: Meu Bem, certeza que é você! Qual é Brenda! Ela tirou as mãos dos olhos de Levir, e então ela diz: "parabéns sua recompensa é um beijo na bochecha." Brenda sutilmente se aproximou do rosto de seu amado e então beijou a bochecha dele, um beijo morno e doce como uma primavera.''']
+
+piadas = ['''Por que o Subaru não consegue encontrar o Rem? Porque ele sempre dá "um passo para a frente" e dois "para trás" no tempo!''',
+'''Qual é o personagem favorito do Subaru em Re:Zero? O Controle Remoto!''',
+'''Por que o Subaru é ótimo em esconde-esconde? Porque sempre que as coisas ficam difíceis, ele reinicia o jogo!''',
+'''O que o Subaru disse para o Puck quando ele congelou? "Você precisa descongelar suas emoções, amigo!"''',
+'''Por que o Otto seria um ótimo comediante? Porque ele sempre tem as melhores "piadas sobre rodas"!''',
+'''Qual é o superpoder secreto da Emilia? Ela pode "resetar" a sua paciência sempre que o Subaru faz algo estúpido!''',
+'''Por que o Subaru não joga videogames? Porque ele já é especialista em "reiniciar"!''',
+'''O que o Subaru disse para a Rem quando ela perguntou se ele a amava? "Claro que sim! Você é minha "Razão de Reiniciar!""''',
+'''Como o Subaru conserta seus problemas? Ele dá um "Ctrl + Z" na situação e espera que tudo melhore!''']
