f5033f01cf8b952c651b9de7c5af847e61fdf883
XarL <131467727+XarLinho@users.noreply.github.com>
2024-01-20 00:29:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ import os
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-barusu = commands.Bot(command_prefix='!rz ', intents=intents)
+bot = commands.Bot(command_prefix='!rz ', intents=intents)
 TOKEN = os.environ['TOKEN']
 
 #COMANDOS
-@barusu.command() #!rz ajuda - O bot irá informar ao usuário os comandos que ele atende.
+@bot.command() #!rz ajuda - O bot irá informar ao usuário os comandos que ele atende.
 async def ajuda(ctx):
     comandos = discord.Embed(
         title = "*Heya!!* Meus Spawnpoint's:",
@@ -29,42 +29,42 @@ async def ajuda(ctx):
     )    
     await ctx.reply(embed=comandos)
 
-@barusu.command() #!rz dado - O bot irá rodar um dado e dirá o resultado.
+@bot.command() #!rz dado - O bot irá rodar um dado e dirá o resultado.
 async def dado(ctx, ld:int):
     lado = random.randint(1,ld)
     await ctx.reply(f'Caiu no {lado}!')
 
-@barusu.command() #!rz dia - O bot irá lhe informar que dia é hoje.
+@bot.command() #!rz dia - O bot irá lhe informar que dia é hoje.
 async def dia(ctx):
     pessoa = ctx.author
     locale.setlocale(locale.LC_TIME, 'pt_BR.utf-8')
     tempo_atual = time.time()
-    tempo_local = time.localtime(tempo_atual_em_segundos)
+    tempo_local = time.localtime(tempo_atual)
     dia = time.strftime("%A", tempo_local)
     await ctx.reply(f'Hoje é {dia} {pessoa.display_name}.')
 
-@barusu.command() #!rz diga (mensagem) - O bot vai repetir o que o user pediu e apagar a msg original.
+@bot.command() #!rz diga (mensagem) - O bot vai repetir o que o user pediu e apagar a msg original.
 async def diga(ctx, *, msg):
     await ctx.message.delete()
     await ctx.send(msg)
 
-@barusu.command() #!rz exposed - O bot vai lhe informar o motivo de seu exposed.
+@bot.command() #!rz exposed - O bot vai lhe informar o motivo de seu exposed.
 async def exposed(ctx, pessoa:discord.Member):
     exposed = random.choice(exposeds).replace('user', pessoa.display_name)
     await ctx.reply(exposed)
 
-@barusu.command() #!rz fanfic - O bot irá contar um fanfic aleatória do servidor.
+@bot.command() #!rz fanfic - O bot irá contar um fanfic aleatória do servidor.
 async def fanfic(ctx):
     fanfic = random.choice(fanfics)
     await ctx.reply(fanfic)
 
-@barusu.command() #!rz hora - O bot irá informar a hora atual.
+@bot.command() #!rz hora - O bot irá informar a hora atual.
 async def hora(ctx:commands.Context):
     pessoa = ctx.author
     hora = time.strftime('%H:%M', time.localtime())
     await ctx.reply(f"{pessoa.display_name} Agora são {hora}.")    
 
-@barusu.command() #!rz limpar (qt) - O bot irá limpar determinado número de mensagens do chat.
+@bot.command() #!rz limpar (qt) - O bot irá limpar determinado número de mensagens do chat.
 async def limpar(ctx, quantidade: int):
     # Verificar se o autor da mensagem tem permissão para limpar mensagens
     if ctx.message.author.guild_permissions.manage_messages:
@@ -76,26 +76,26 @@ async def limpar(ctx, quantidade: int):
         # Se o autor não tiver permissões adequadas, enviar uma mensagem de aviso
         await ctx.send("Você não tem permissão para limpar mensagens.")
 
-@barusu.command() #!rz mandar (id_canal) (fala) - O bot irá mandar o que você disse no canal escolhido.
+@bot.command() #!rz mandar (id_canal) (fala) - O bot irá mandar o que você disse no canal escolhido.
 async def mandar(ctx, id_canal:int, *, fala):
-    canal = barusu.get_channel(id_canal)
+    canal = bot.get_channel(id_canal)
 
     if canal is None:
         await ctx.send('Canal não encontrado!')
         return
     await canal.send(fala)
 
-@barusu.command() #!rz piada - O bot irá contar uma piada aleatória sobre Re:Zero.
+@bot.command() #!rz piada - O bot irá contar uma piada aleatória sobre Re:Zero.
 async def piada(ctx):
     piada = random.choice(piadas)
     await ctx.reply(piada)
 
-@barusu.command() #!rz saudação - O bot irá saudar o usuário.
+@bot.command() #!rz saudação - O bot irá saudar o usuário.
 async def saudar(ctx):
     user = ctx.author
-    await ctx.reply(f'> Heya {user.display_name}! Aqui é o Barusu. Retornando pela morte estou aqui para encarar qualquer desafio. A vida é cheia de reviravoltas, mas não se preocupe, estou sempre pronto para recomeçar!')
+    await ctx.reply(f'> Heya {user.display_name}! Aqui é o bot. Retornando pela morte estou aqui para encarar qualquer desafio. A vida é cheia de reviravoltas, mas não se preocupe, estou sempre pronto para recomeçar!')
 
-@barusu.command() #!rz spam - O bot irá spamar quantas mensagens você quiser no chat.
+@bot.command() #!rz spam - O bot irá spamar quantas mensagens você quiser no chat.
 async def spam(ctx, qt:int, *, frase):
     i = 0
     await ctx.message.delete()
@@ -103,10 +103,10 @@ async def spam(ctx, qt:int, *, frase):
         await ctx.send(frase)
 
 #EVENTOS
-@barusu.event #Quando alguem escrever uma mensagem que contenha a palavra rem o usuário o responde.
+@bot.event #Quando alguem escrever uma mensagem que contenha a palavra rem o usuário o responde.
 async def on_message(pal:discord.Message):
-    await barusu.process_commands(pal)
-    if pal.author == barusu.user:
+    await bot.process_commands(pal)
+    if pal.author == bot.user:
         return
     if 'rem' in pal.content.lower():
         await pal.reply('REM??????')
@@ -115,9 +115,9 @@ async def on_message(pal:discord.Message):
 
 @bot.event #Quando o bot estiver online irá aparecer no console.
 async def on_ready():
-    print(f'{barusu.user} está online!')
+    print(f'{bot.user} está online!')
     #canal = bot.get_channel(757649361248190546)
     #await canal.send('Spawnpoint!')
 
 #VALIDAR TOKEN
-barusu.run(TOKEN)
+bot.run(TOKEN)
